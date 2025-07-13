79d3ba7a987b51c0a9a3c4a68cd1fe4fe75b561b
Simon Halsey <simon@thehalseys.uk>
2025-07-13 14:00:00 +0100

    Added additional person fields

--- src/classe.abstractions/Entities/Person.cs
+++ src/classe.abstractions/Entities/Person.cs
@@ -14,6 +14,24 @@
 
         public float Balance { get; set; }
 
+        public string? Address { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string? Occupation { get; set; }
+
+        public string? EmergencyContact { get; set; }
+
+        public string? EmergencyContactNumber { get; set; }
+
+        public string? JoiningNotes { get; set; }
+
+        public string? Notes { get; set; }
+
+        public DateTime? ConsentDate { get; set; }
+
         public byte[]? RowVersion { get; init; }
         public ICollection<Session> Sessions { get; set; } = [];
 
@@ -21,4 +39,34 @@
 
         public ICollection<Payment> Payments { get; set; } = [];
     }
+
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+    }
+
+    /*
+     * joining notes
+     * --------------
+     * regular exercise
+     * what other exercise
+     * goals
+     * existing medical conditions
+     * joint injuries
+     * additional needs
+     * doctor recommendation
+     * pain on physical activity
+     * chest pain
+     * dizziness
+     * doctor prescribed drugs
+     * bone or join problems
+     * epilepsy
+     * diabetes
+     * asthma
+     * any reason not to do physical activity
+     * discussed above with doctor
+     * additional info
+     */
 }
